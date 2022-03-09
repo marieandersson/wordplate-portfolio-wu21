ac8bf3fc572bb669eb33d83e6e33846122d31618
Marie Andersson <marie.andersson2303@gmail.com>
2022-03-09 21:12:55 +0100

    Fix syntax in block template

--- public/themes/portfolio/block-templates/latest-posts.php
+++ public/themes/portfolio/block-templates/latest-posts.php
@@ -15,20 +15,21 @@ $latestPosts = get_posts($args);
 
 ?>
 
-<div <?php if (!empty($backgroundColor)):?> style="background-color:<?= $backgroundColor; ?>;"<?php endif; ?>?>
-<div class="grid gap-5 grid-cols-3 mt-8 not-prose">
-    <?php foreach($latestPosts as $post): ?>
-        <a href="<?= get_the_permalink($post); ?>">
-            <?php if ($image = get_the_post_thumbnail($post, 'large')): ?>
-                <?= $image; ?>
-            <?php endif; ?>
-            <h3 class="text-2xl"><?= get_the_title($post); ?></h3>
-        </a>
-    <?php endforeach; ?>
-</div>
-
-<?php if (!empty($latestPostsArchiveLink)): ?>
-    <div class="mt-8 not-prose">
-        <a class="bg-teal-900 text-white px-3 py-2" href="<?= esc_url($latestPostsArchiveLink); ?>"><?= $latestPostsLinkLabel; ?></a>
+<div <?php if (!empty($backgroundColor)):?> style="background-color:<?= $backgroundColor; ?>;"<?php endif; ?>>
+    <div class="grid gap-5 grid-cols-3 mt-8 not-prose">
+        <?php foreach($latestPosts as $post): ?>
+            <a href="<?= get_the_permalink($post); ?>">
+                <?php if ($image = get_the_post_thumbnail($post, 'large')): ?>
+                    <?= $image; ?>
+                <?php endif; ?>
+                <h3 class="text-2xl"><?= get_the_title($post); ?></h3>
+            </a>
+        <?php endforeach; ?>
     </div>
-<?php endif; ?>
+
+    <?php if (!empty($latestPostsArchiveLink)): ?>
+        <div class="mt-8 not-prose">
+            <a class="bg-teal-900 text-white px-3 py-2" href="<?= esc_url($latestPostsArchiveLink); ?>"><?= $latestPostsLinkLabel; ?></a>
+        </div>
+    <?php endif; ?>
+</div>
